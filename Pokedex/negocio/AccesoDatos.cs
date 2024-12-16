e44299fa64fb2c7807376e4c9a9d0327bfab7fb5
Horacio <horacioanadon00@gmail.com>
2024-12-16 19:56:25 -0300

    desarrollo clase acceso a datos

--- Pokedex/negocio/AccesoDatos.cs
+++ Pokedex/negocio/AccesoDatos.cs
@@ -9,6 +9,66 @@ namespace negocio
 {
     public class AccesoDatos
     {
-        
+        //propiedades necesarias para la conexion
+        SqlConnection conexion;
+        SqlCommand comando;
+        SqlDataReader reader;
+
+        //acceso al lector
+        public SqlDataReader Reader
+        {
+            get { return reader; }
+        }
+        //constructor clase. Instancia la conexión configurada y el comando.
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS01 ; database=POKEDEX_DB ; integrated security=true");
+            comando = new SqlCommand();
+        }
+        //método setear consulta
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        //metodo ejecutar lectura
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                reader = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        //metodo ejecutar acción contra la base de datos 
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        //cerrar conexion a db
+        public void cerrarConexion()
+        {
+            if (reader != null)
+            {
+                reader.Close();
+            }
+            conexion.Close();
+        }
     }
 }
